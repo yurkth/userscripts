2274a1db80e9e8bc441469bfb5fa781e918a0009
torin <yurkth@gmail.com>
2023-10-22 21:56:50 +0900

    ⚡️ YTSheet Old Expがキャラシの中でも動作するように対応

--- ytsheet/exp.user.js
+++ ytsheet/exp.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         YTSheet Old Exp
 // @namespace    https://github.com/yurkth/userscripts
-// @version      0.1.0
+// @version      0.2.0
 // @description  ゆとシート(DX3rd)の経験点表示を「+N」から「M」(M=130+N)に戻すスクリプト
 // @author       torin
+// @match        https://yutorize.2-d.jp/ytsheet/dx3rd/?id=*
 // @match        https://yutorize.2-d.jp/ytsheet/dx3rd/?mode=mylist
 // @match        https://yutorize.2-d.jp/ytsheet/dx3rd/?group=*
 // @updateURL    https://github.com/yurkth/userscripts/raw/master/ytsheet/exp.user.js
@@ -14,7 +15,18 @@
 
 (function () {
   'use strict';
-  document.querySelectorAll("td.exp").forEach(td => {
-    td.textContent = eval(`130${td.textContent}`);
-  });
+  if (window.location.search.slice(1).split("=")[0] == "id") { // キャラシ
+    replaceEvalExp(document.querySelector("section#exp>dl>dd"));
+  } else { // キャラシ一覧
+    document.querySelectorAll("td.exp").forEach(td => {
+      replaceEvalExp(td);
+    });
+  }
 })();
+
+function replaceEvalExp(elem) {
+  console.log(elem);
+  if (elem.textContent[0] == "+") {
+    elem.textContent = eval(`130${elem.textContent}`);
+  }
+}
